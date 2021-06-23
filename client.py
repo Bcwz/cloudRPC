d4e9e15c35a8f179f1098c6fca7868a1efd22d06
Bernie <bernie.c.weizhuang@gmail.com>
2021-06-24 01:44:48 +0800

    added node_reply

--- client.py
+++ client.py
@@ -25,14 +25,6 @@ class trafficClient(object):
         #id to identify node
         self.id = id
 
-    # def get_url(self, message):
-    #     """
-    #     Client function to call the rpc for GetServerResponse
-    #     """
-    #     message = pb2.Message(message=message)
-    #     print(f'{message}')
-    #     return self.stub.GetServerResponse(message)
-
     def ping_server(self,id,message):
         """ Ping from traffic light node to traffic light server / controller """
         ping = pb2.Message(message= "{0} pinged : {1}".format(id,message))
@@ -43,8 +35,19 @@ class trafficClient(object):
     def ping_node(self,requestId,message,responseId):
         """ Ping from traffic light node to traffic light node """
         ping = pb2.Message(message= "{0} pinged : {1} to {2}".format(requestId,message,responseId))
-        print(f'{ping}')
 
+        #Getting a node reply if dead or alive
+        print(client.node_reply(responseId))
+        #print(f'{ping}')
+
+    def node_reply(self,responseId):
+        alive = True
+        if alive:
+            alive_message = ('Yes, {0} is alive'.format(responseId))
+            return alive_message
+        else:
+            dead_message = ('No, {0} is dead'.format(responseId))
+            return dead_message
 
 if __name__ == '__main__':
     client = trafficClient(id="testXXXXX")
@@ -52,24 +55,17 @@ if __name__ == '__main__':
     south = trafficClient(id="south")
     east = trafficClient(id="east")
     west = trafficClient(id="west")
-
-
     while True:
-        #ping server
-        # reply = client.ping_server(id=client.id,message="Server are you alive?")
-        # print(f'{reply}')
-
         #Ping node - threaded
-        ping_thread = threading.Thread(target=client.ping_node, args=(north.id,"THREADMESSAGE","south"))
+        ping_thread = threading.Thread(target=client.ping_node, args=(north.id,"Are you alive?","south"))
         ping_thread.start()
-
-
         # time.sleep for fixed interval
         time.sleep(5)
 
 
 
-    # result = client.get_url(message="Hello Server you there?")
-    # x = threading.Thread(target=client.get_url, args=('thread',))
-    # result = x.start()
-    # print(f'{result}')
+
+
+        #ping server
+        # reply = client.ping_server(id=client.id,message="Server are you alive?")
+        # print(f'{reply}')

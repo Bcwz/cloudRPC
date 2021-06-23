5beca518d7137d9be44e4f63b5f8d414b40e0908
Bernie <bernie.c.weizhuang@gmail.com>
2021-06-24 00:54:30 +0800

    added pings to node

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import grpc
 import trafficLight_pb2_grpc as pb2_grpc
 import trafficLight_pb2 as pb2
 import threading
+import os
 
 
 class trafficClient(object):
@@ -21,18 +22,42 @@ class trafficClient(object):
         # bind the client and the server
         self.stub = pb2_grpc.trafficLightStub(self.channel)
 
-    def get_url(self, message):
-        """
-        Client function to call the rpc for GetServerResponse
-        """
-        message = pb2.Message(message=message)
-        print(f'{message}')
-        return self.stub.GetServerResponse(message)
+        self.id = ''
 
+    # def get_url(self, message):
+    #     """
+    #     Client function to call the rpc for GetServerResponse
+    #     """
+    #     message = pb2.Message(message=message)
+    #     print(f'{message}')
+    #     return self.stub.GetServerResponse(message)
+
+    def ping_nodes(self,id,message):
+        """ Ping traffic light nodes within traffic light junction """
+        ping = pb2.Message(message= "{0} pinged : {1}".format(id,message))
+        print(f'{ping}')
+        return self.stub.GetServerResponse(ping)
+
+    def node_north():
+        """ North traffic node"""
+        id = "north"
+
+    def node_south():
+        """ South traffic node"""
+        id = "south"
+        
+    def node_east():
+        """ east traffic node"""
+        id = "east"
+
+    def node_west():
+        """ West traffic node"""
+        id = "west"
 
 if __name__ == '__main__':
     client = trafficClient()
     # result = client.get_url(message="Hello Server you there?")
-    x = threading.Thread(target=client.get_url, args=('thread',))
-    result = x.start()
+    # x = threading.Thread(target=client.get_url, args=('thread',))
+    # result = x.start()
     # print(f'{result}')
+    client.ping_nodes(id="north",message="Are you alive?")

da999398965472cad6a52a7fdb4dd5938b3fe69a
09loluic <mrvincent963@gmail.com>
2021-07-03 21:50:34 +0800

    Delete Cloud_Prototype/JunctionA directory

--- Cloud_Prototype/JunctionA/Run_ME.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start python Traffic_Controller.py JunctionA-Controller JunctionB-Controller 50055 50060
-start python Traffic_Light.py JunctionA TL-A TL-B 50051 50052
-start python Traffic_Light.py JunctionA TL-B TL-C 50052 50053
-start python Traffic_Light.py JunctionA TL-C TL-D 50053 50054
-start python Traffic_Light.py JunctionA TL-D TL-A 50054 50051
-pause
